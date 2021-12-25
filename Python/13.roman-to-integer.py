8be4d7097f49c1ee5b9e397eeed6c1264163cd4b
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2021-12-25 13:55:29 +0600

    accepted - 66.68% faster that others submission

--- Python/13.roman-to-integer.py
+++ Python/13.roman-to-integer.py
@@ -8,9 +8,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         values = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100,'D':500,'M':1000}
+        orders = {'I':1, 'V':2, 'X':3, 'L':4, 'C':5,'D':6,'M':7}
         total = 0
-        for i in s:
-            total += values[i]
-        return total
+        for i in range(len(s)-1):
+            roman = s[i]
+            
+            if orders[s[i]] >= orders[s[i+1]]:
+                total += values[s[i]]
+            else:
+                total -= values[s[i]]
+
+        return total+values[s[-1]]
         
 # @lc code=end
