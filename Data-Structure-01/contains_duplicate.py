f80ddfe2cbf1f517b4b3ee4e4542feeb85b2acf0
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-01-05 07:40:23 +0600

    moved files to new dir

--- Data-Structure-01/contains_duplicate.py
+++
@@ -1,8 +0,0 @@
-# https://leetcode.com/problems/contains-duplicate/
-
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        x = set(nums)
-        if len(nums) == len(x):
-            return False
-        return True
