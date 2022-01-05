d3ced7b91e2ef22d3fe3d64be9f4e34b00e50669
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-01-05 11:09:30 +0600

    accepted

--- Python/350.intersection-of-two-arrays-ii.py
+++ Python/350.intersection-of-two-arrays-ii.py
@@ -7,8 +7,21 @@
 # @lc code=start
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums3 = [x for x in nums1 if x in nums2]
-        return nums3
+        output = []
+        dic = {}
+        # counting total number of appearance of an element
+        for ele in nums2:
+            if ele not in dic:
+                dic[ele] = 1
+            else:
+                dic[ele] += 1
+        
+        for i in nums1:
+            # if there are enough number of element then append it to result [], and decrease number of appearance by 1
+            if i in nums2 and dic[i] > 0:
+                output.append(i)
+                dic[i] -= 1
+        return output
         
         
 # @lc code=end
