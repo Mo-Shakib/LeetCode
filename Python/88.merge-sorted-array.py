475821e1494983d740b30362314073e9bd86bcf8
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-01-05 08:36:16 +0600

    accepted

---
+++ Python/88.merge-sorted-array.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m, len(nums1)):
+            nums1[i] = nums2[i-m]
+        nums1.sort()
+        
+# @lc code=end
+
