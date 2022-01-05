47cb0c7ede1d5bc1fb231a84c4bcf6cfed0f20ba
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-01-05 06:29:16 +0600

    comments added

--- Python/997.find-the-town-judge.py
+++ Python/997.find-the-town-judge.py
@@ -7,13 +7,18 @@
 # @lc code=start
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trusted = {}
+        trusted = {} # to store key value pairs
+        # there is only one person and he does not trust himself
         if len(trust) == 0 and n == 1:
             return 1
+        # there are more than one perosn and nobody trust anyone!
         if n > 1 and len(trust) == 0:
             return -1
         
+        # looping through the list and getting the trust values of every person. If a person is trusted by someone then his trust value increases else decreases.
+        
         for (a, b) in trust:
+            
             if a not in trusted:
                 trusted[a] = -1
             else:
@@ -24,6 +29,7 @@ class Solution:
             else:
                 trusted[b] += 1
         
+        # if a person is trusted by n-1 people then he will be the judge, else there is no judge!
         for key, value in trusted.items():
             if value == n -1:
                 return key
