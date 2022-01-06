3c4522fb948b2224dab9c28971dd09b8c2f2b9ff
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-01-06 19:04:16 +0600

    accepted

---
+++ Python/709.to-lower-case.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=709 lang=python3
+#
+# [709] To Lower Case
+#
+
+# @lc code=start
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+        
+# @lc code=end
+
