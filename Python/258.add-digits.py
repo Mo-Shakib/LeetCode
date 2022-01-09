c3d0867211ef93958bb5af6baa8f88661cca0d26
Mohammad Shakib <50780268+Mo-Shakib@users.noreply.github.com>
2022-01-09 07:59:41 +0600

    accepted

--- Python/258.add-digits.py
+++ Python/258.add-digits.py
@@ -9,7 +9,10 @@ class Solution:
     def addDigits(self, num: int) -> int:
         if num <= 9:
             return num
+        if num % 9 == 0:
+            return 9
         return num % 9
-        
+    # Time complexity: O(1)
+    
 # @lc code=end
 
