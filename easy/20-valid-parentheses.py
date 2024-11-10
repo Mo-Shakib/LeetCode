b7823adc6b7643f8ddf98a65ebeb5c95ed54a68b
Mo-Shakib <mo.shakib.official@gmail.com>
2024-11-10 21:43:23 +0600

    updated

--- easy/20-valid-parentheses.py
+++ easy/20-valid-parentheses.py
@@ -74,28 +74,18 @@ class Solution:
             return False
         
         open = ['(','{','[']
-        close = [')','}',']']
-
         stack = []
-        for i in s:
-            if i in open:
-                stack.append(i)
-                flag = True
-            
-            elif i in close and len(stack) != 0:
-                if stack[-1] == '(' and i == ')':
-                    stack.pop()
-                elif stack[-1] == '{' and i == '}':
-                    stack.pop()
-                elif stack[-1] == '[' and i == ']':
-                    stack.pop()
-                return False
-            elif len(stack) == 0 and i in close:
-                return False
-        
-        if len(stack) == 0:
-            return True
-        return False
+        for braket in s:
+            if braket in open:
+                stack.append(braket)
+            else:
+                if len(stack) == 0:
+                    return False
+                popped = stack.pop()
+                if (braket == ')' and popped != '(') or (braket == '}' and popped != '{') or (braket == ']' and popped != '['):
+                    return False
+        return (len(stack) == 0)
+              
         
 # @lc code=end
 
